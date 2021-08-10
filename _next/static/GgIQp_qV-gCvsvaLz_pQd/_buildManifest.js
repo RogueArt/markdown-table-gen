5fb851e207e4bc1f9c7ab66e9739cb597f936bc2
RogueArt <RogueArt@users.noreply.github.com>
2021-08-10 03:18:09 +0000

    deploy: 054de19b94851c01f79d6e4e89c4c71a88eef98c

---
+++ _next/static/GgIQp_qV-gCvsvaLz_pQd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b33217a5d7f07dfdec32.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
