08dec10d316dbf5e8c826179485dacb64b1c4cc2
RogueArt <altoraries@gmail.com>
2021-08-09 19:44:19 -0700

    Set up continuous deployment for my Next app

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  // Use this prefix when using the default github.io link
+  assetPrefix: isProduction ? '/markdown-table-gen' : '',
 }
