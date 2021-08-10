868f3ec09ff62c7cfe4c00fec8befef2e59f5359
RogueArt <altoraries@gmail.com>
2021-08-09 19:41:53 -0700

    Added useHotkeys hook to website

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { useHotkeys } from 'react-hotkeys-hook'
 import { formatTableAsStr, createInitialArray } from '../utils/formatTable.js'
 
 export default function Home() {
@@ -12,6 +13,48 @@ export default function Home() {
   // State is the table contents
   const initialArray = createInitialArray(height, width)
   const [tableContents, setTableContents] = useState(initialArray)
+
+  // Store table contents as string
+  const tableContentsAsStr = formatTableAsStr(tableContents)
+
+  // Handle user shortcuts
+  useHotkeys('shift+left', () => handleSizeChange(height, width - 1))
+  useHotkeys('shift+right', () => handleSizeChange(height, width + 1))
+  useHotkeys('shift+down', () => handleSizeChange(height + 1, width))
+  useHotkeys('shift+up', () => handleSizeChange(height - 1, width))
+  useHotkeys('shift+c', () => copyContentsToClipboard())
+
+  // Write prettified markdown to the clipboard
+  function copyContentsToClipboard() {
+    navigator.clipboard.writeText(tableContentsAsStr)
+    console.log('Copied!')
+  }
+
+  // Resize the array if the size changes
+  function handleSizeChange(newHeight, newWidth) {
+    if (newHeight === height && newWidth === width) return
+
+    // Update height and width
+    setHeight(height => newHeight)
+    setWidth(width => newWidth)
+
+    // Create a new empty array
+    const newTableContents = createInitialArray(newHeight, newWidth)
+
+    // We are copying over the bare minimum needed
+    const numRows = Math.min(height, newHeight)
+    const numCols = Math.min(width, newWidth)
+
+    // Copy over contents to the new array
+    for (let x = 1; x < numRows; x += 1) {
+      for (let y = 0; y < numCols; y += 1) {
+        newTableContents[x][y] = tableContents[x][y]
+      }
+    }
+
+    // Set new array contents, re-render
+    setTableContents(tableContents => [...newTableContents])
+  }
 
   // Every time content changes, update that element, and set new state
   function handleContentChange(e, [x, y]) {
@@ -75,7 +118,7 @@ export default function Home() {
         {/* Copy-pastable output */}
         <textarea
           className="textarea"
-          value={formatTableAsStr(tableContents)}
+          value={tableContentsAsStr}
           readOnly
         ></textarea>
       </main>
@@ -84,7 +127,9 @@ export default function Home() {
       <footer className="footer">
         <p className="footer__contact">
           If you found this tool useful, check out my other projects{' '}
-          <a className="footer__link" href="https://github.com/RogueArt">here!</a>
+          <a className="footer__link" href="https://github.com/RogueArt">
+            here!
+          </a>
         </p>
       </footer>
     </div>
